0ee07689237ced0dd53df240e494bf39a87fcd09
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2021-12-03 13:10:53 +0200

    up

--- resources/scripts/layout/header-large_menu.js
+++ resources/scripts/layout/header-large_menu.js
@@ -1,44 +1,50 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  function DesktopHandler (e) {
+  const isTouch = () => 'ontouchstart' in window || window?.DocumentTouch && document instanceof window.DocumentTouch || navigator.maxTouchPoints > 0 || window.navigator?.msMaxTouchPoints > 0;
+
+  function getChild(el) {
+    const column = el.dataset.column;
+    const ul = el.dataset.ul;
+    const li = el.dataset.li;
+
+    return document.querySelector(`.header-large_menu-body ul[data-parent="${column}.${ul}.${li}"]`);
+  }
+
+  function getColumnNumber(el) {
+    if (el.closest('.column').classList.contains('column-2')) return 2;
+    else if (el.closest('.column').classList.contains('column-3')) return 3;
+
+    return 1;
+  }
+
+  function DesktopHandler(e) {
     if (window.isTablet()) return true;
 
     const liElem = e.target.closest('li');
-
-    const column = liElem.dataset.column;
-    const ul = liElem.dataset.ul;
-    const li = liElem.dataset.li;
-
-    let columnNumber = 1;
-
-    if (liElem.closest('.column').classList.contains('column-2')) columnNumber = 2;
-    else if (liElem.closest('.column').classList.contains('column-3')) columnNumber = 3;
-
-    const child = document.querySelector(`.header-large_menu-body ul[data-parent="${column}.${ul}.${li}"]`);
+    const column = getColumnNumber(liElem);
+    const child = getChild(liElem);
 
     if (child) {
       if (isTouch() && 'A' === e.target.tagName) e.preventDefault();
 
       liElem.closest('.column').querySelector('li.active')?.classList.remove('active');
-      child.closest('.column').querySelector('ul.open')?.classList.remove('open');
+      child.closest('.column').querySelector('ul:not(.hidden)')?.classList.add('hidden');
 
-      if (1 === columnNumber) {
+      if (1 === +column) {
         document.querySelector('.header-large_menu-body .column-2 li.active')?.classList.remove('active');
-        document.querySelector('.header-large_menu-body .column-3 ul.open')?.classList.remove('open');
+        document.querySelector('.header-large_menu-body .column-3 ul:not(.hidden)')?.classList.add('hidden');
       }
 
       liElem.classList.add('active');
-      child.classList.add('open');
+      child.classList.remove('hidden');
     }
     else {
-      for (let i = columnNumber + 1; i <= 3; i++) {
+      for (let i = +column + 1; i <= 3; i++) {
         document.querySelector(`.header-large_menu-body .column-${i - 1} li.active`)?.classList.remove('active');
-        document.querySelector(`.header-large_menu-body .column-${i} ul.open`)?.classList.remove('open');
+        document.querySelector(`.header-large_menu-body .column-${i} ul:not(.hidden)`)?.classList.add('hidden');
       }
     }
   }
-
-  const isTouch = () => 'ontouchstart' in window || window?.DocumentTouch && document instanceof window.DocumentTouch || navigator.maxTouchPoints > 0 || window.navigator.msMaxTouchPoints > 0;
 
   for (let item of document.querySelectorAll('.header-large_menu-body .column li')) {
     item.addEventListener('mouseenter', function (e) {
@@ -54,12 +60,12 @@ document.addEventListener('DOMContentLoaded', function () {
     if (isTouch()) return true;
     if (window.isTablet()) return true;
 
-    for (let li of document.querySelectorAll(`.column li.active`)) {
+    for (let li of document.querySelectorAll('.column li.active')) {
       li.classList.remove('active');
     }
 
-    for (let ul of document.querySelectorAll(`.column ul.open`)) {
-      ul.classList.remove('open');
+    for (let ul of document.querySelectorAll('.column:not(.column-1) ul:not(.hidden)')) {
+      ul.classList.add('hidden');
     }
   });
 
@@ -70,34 +76,49 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (!liElem) return true;
 
-    const column = liElem.dataset.column;
-    const ul = liElem.dataset.ul;
-    const li = liElem.dataset.li;
-
-    let columnNumber = 1;
-
-    if (liElem.closest('.column').classList.contains('column-2')) columnNumber = 2;
-    else if (liElem.closest('.column').classList.contains('column-3')) columnNumber = 3;
-
-    const child = document.querySelector(`.header-large_menu-body ul[data-parent="${column}.${ul}.${li}"]`);
+    const column = getColumnNumber(liElem);
+    const child = getChild(liElem);
 
     if (child) {
       if ('A' === e.target.tagName) e.preventDefault();
 
-      if (1 === columnNumber) {
+      if (1 === +column) {
         document.querySelector('.header-large_menu-tablet').classList.add('hidden');
         liElem.closest('ul').classList.add('hidden');
       }
       else {
         liElem.closest('.column').querySelector('.tablet-sub-menu-head').classList.add('hidden');
-        liElem.closest('ul').classList.remove('open');
+        liElem.closest('ul').classList.add('hidden');
       }
 
       const head = child.closest('.column').querySelector('.tablet-sub-menu-head');
       head.querySelector('span').textContent = liElem.querySelector('a').textContent;
       head.classList.remove('hidden');
 
-      child.classList.add('open');
+      child.classList.remove('hidden');
+    }
+  });
+
+  document.querySelector('.header-large_menu-body').addEventListener('click', function (e) {
+    const head = e.target.closest('.tablet-sub-menu-head');
+    const span = e.target.closest('.tablet-sub-menu-head span');
+    const svg = e.target.closest('.tablet-sub-menu-head svg');
+
+    if (!span && !svg) return true;
+
+    const list = head.closest('.column').querySelector('ul:not(.hidden)');
+    const [column, ul] = list.dataset.parent.split('.');
+
+    head.classList.add('hidden');
+    list.classList.add('hidden');
+
+    if (1 === +column) {
+      document.querySelector('.header-large_menu-tablet').classList.remove('hidden');
+      document.querySelector('.header-large_menu-body .column-1 ul').classList.remove('hidden');
+    }
+    else {
+      document.querySelector(`.header-large_menu-body .column-${column} .tablet-sub-menu-head`).classList.remove('hidden');
+      document.querySelector(`.header-large_menu-body .column-${column} .ul-${ul}`).classList.remove('hidden');
     }
   });
 
