ece301254b49e42522fd49a76296fe9e904ee86e
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2021-12-17 22:07:34 +0200

    up

--- resources/scripts/page/where_to_buy/top.js
+++ resources/scripts/page/where_to_buy/top.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+document.addEventListener('submit', function (e) {
+  const form = e.target.closest('.where_to_buy.form');
 
+  if (!form) return true;
+
+  e.preventDefault();
+
+  console.log('filter');
+});
 
 });
