b9079bc2442ca5ae5c5f09cb239bd46c6f0beb86
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2021-12-28 22:42:11 +0200

    up

--- resources/scripts/page/collection/cards.js
+++ resources/scripts/page/collection/cards.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   document.addEventListener('click', function (e) {
+    const image = e.target.closest('.modal-popup.collection_modal .img-small:not(.active)');
+
+    if (!image) return true;
+
+    document.querySelector('.modal-popup.collection_modal .img-small.active').classList.remove('active');
+    document.querySelector('.modal-popup.collection_modal .images .img-big img').src = image.querySelector('img').src;
+    image.classList.add('active');
+  });
+
+  document.addEventListener('click', function (e) {
     const button = e.target.closest('.collection-wrapper .collection_cards .btn-popup');
 
     if (!button) return true;
@@ -15,21 +25,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const images = Array.from(card.querySelectorAll('.images li')).map((i) => (i.textContent).trim());
     const properties = Array.from(card.querySelectorAll('.properties li')).map((i) => (i.textContent).trim());
 
-    console.log(collection);
-    console.log(title);
-    console.log(text);
-    console.log(link);
-    console.log(properties);
-
-    const img0 = document.querySelector('.modal-popup.collection_modal .images .img-0');
-    const img1 = document.querySelector('.modal-popup.collection_modal .images .img-1');
-    const img2 = document.querySelector('.modal-popup.collection_modal .images .img-2');
-    const imgBig = document.querySelector('.modal-popup.collection_modal .images .img-big');
+    const img0 = document.querySelector('.modal-popup.collection_modal .images .img-0 img');
+    const img1 = document.querySelector('.modal-popup.collection_modal .images .img-1 img');
+    const img2 = document.querySelector('.modal-popup.collection_modal .images .img-2 img ');
+    const imgBig = document.querySelector('.modal-popup.collection_modal .images .img-big img');
 
     img0.src = images[0];
     img1.src = images[1];
     img2.src = images[2];
     imgBig.src = images[0];
+
+    document.querySelector('.modal-popup.collection_modal .texts .text').innerHTML = text;
+    document.querySelector('.modal-popup.collection_modal .texts .collection-title').textContent = collection;
+    document.querySelector('.modal-popup.collection_modal .texts .product-title').textContent = title;
+
+    console.log(properties);
+    console.log(link);
 
     window.openModal('collection_modal');
   });
