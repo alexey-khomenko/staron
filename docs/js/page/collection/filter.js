16309ff5779cd5b8e406c0e8e4d1bbe1a4832028
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2021-12-29 22:17:07 +0200

    up

--- docs/js/page/collection/filter.js
+++ docs/js/page/collection/filter.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('collection/filter');
+
+  document.addEventListener('click', function (e) {
+    const button = e.target.closest('.collection_filter .btn-filter-title');
+
+    if (!button) return true;
+
+    document.querySelector('.collection_filter .body-wrapper').classList.toggle('md-hidden');
+    document.querySelector('.collection_filter .filter_reset').classList.toggle('md-hidden');
+  });
+
 });
--- resources/scripts/page/catalog/filter.js
+++ resources/scripts/page/catalog/filter.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('catalog/filter');
+
+  document.addEventListener('click', function (e) {
+    const button = e.target.closest('.catalog_filter .btn-filter-title');
+
+    if (!button) return true;
+
+    document.querySelector('.catalog_filter .body-wrapper').classList.toggle('md-hidden');
+    document.querySelector('.catalog_filter .filter_reset').classList.toggle('md-hidden');
+  });
+
 });
