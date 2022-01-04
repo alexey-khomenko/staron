4b723a93860c3f38ff61e8c41757d510d46204f6
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2022-01-04 18:02:24 +0200

    up

--- docs/js/page/index/scroll.js
+++ docs/js/page/index/scroll.js
@@ -3,7 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
   document.addEventListener('click', function (e) {
     if (!e.target.closest('.scroll-btn')) return true;
 
-    document.querySelector('.benefits').scrollIntoView({behavior: 'smooth'});
+    const topOffset = document.querySelector('.header').offsetHeight;
+    const elementPosition =  document.querySelector('.benefits').getBoundingClientRect().top;
+
+    window.scrollBy({top: elementPosition - topOffset, behavior: 'smooth'});
   });
 
 });
