598a93b59494b347c2b99d26c934ed5bfcdcf49b
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2022-01-21 18:06:37 +0200

    up

--- docs/js/page/3d/bottom.js
+++ docs/js/page/3d/bottom.js
@@ -1,3 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('bottom');
+  const selects = document.querySelector('.visualisation-3d.bottom .selects');
+
+  document.addEventListener('click', function (e) {
+    const placeHolder = e.target.closest('.visualisation-3d.bottom .select-placeholder-opened');
+
+    if (!placeHolder) return true;
+
+    placeHolder.closest('.select').classList.remove('active');
+  });
+
+  document.addEventListener('click', function (e) {
+    const placeHolder = e.target.closest('.visualisation-3d.bottom .select-placeholder-closed');
+
+    if (!placeHolder) return true;
+
+    const newSelect = placeHolder.closest('.select');
+    const oldSelect = selects.querySelector('.select.active');
+
+    newSelect.classList.add('active');
+
+    if (oldSelect) oldSelect.classList.remove('active');
+  });
+
+  document.addEventListener('click', function (e) {
+    const select = e.target.closest('.visualisation-3d.bottom .select');
+
+    if (select) return true;
+
+    const oldSelect = selects.querySelector('.select.active');
+
+    if (oldSelect) oldSelect.classList.remove('active');
+  });
+
 });
