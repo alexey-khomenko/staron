66a824f0528f01525794bcac0e635da1d9b9b986
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2022-01-21 15:02:36 +0200

    up

--- docs/js/page/3d/top.js
+++ docs/js/page/3d/top.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('top');
+  function setActiveCard(newCard) {
+    if (!newCard) return true;
+
+    const oldCard = newCard.closest('.cards').querySelector('.card.active');
+
+    newCard.classList.add('active');
+
+    document.querySelector('.visualisation-3d .wrapper-3d img').src = newCard.dataset.srcLarge;
+    document.querySelector('.visualisation-3d .to-product').href = newCard.dataset.productLink;
+
+    if (oldCard) oldCard.classList.remove('active');
+  }
+
+  const cards = document.querySelectorAll('.visualisation-3d.top .card');
+  setActiveCard(cards[0]);
+
+  document.addEventListener('click', function (e) {
+    const newCard = e.target.closest('.visualisation-3d.top .card:not(.active)');
+    setActiveCard(newCard);
+  });
 });
