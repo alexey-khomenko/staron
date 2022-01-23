a81a09fda916edae7cad4b785db3bf542928386c
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2022-01-23 19:58:12 +0200

    up

--- docs/js/page/3d/top.js
+++ docs/js/page/3d/top.js
@@ -1,22 +1,69 @@
 document.addEventListener('DOMContentLoaded', function () {
-  function setActiveCard(newCard) {
+
+  const selectCollection = document.querySelector('.visualisation-3d.bottom .select-collection');
+  const selectColor = document.querySelector('.visualisation-3d.bottom .select-color');
+  const selectProduct = document.querySelector('.visualisation-3d.bottom .select-product');
+  const img3d = document.querySelector('.visualisation-3d .wrapper-3d img');
+  const prodLink = document.querySelector('.visualisation-3d .to-product');
+
+  function run(newCard) {
     if (!newCard) return true;
 
-    const oldCard = newCard.closest('.cards').querySelector('.card.active');
-
-    newCard.classList.add('active');
-
-    document.querySelector('.visualisation-3d .wrapper-3d img').src = newCard.dataset.srcLarge;
-    document.querySelector('.visualisation-3d .to-product').href = newCard.dataset.productLink;
-
-    if (oldCard) oldCard.classList.remove('active');
+    setActiveCard(newCard);
+    window.resetSelectColor();
+    setSelectColor(newCard);
+    window.resetSelectCollection();
+    window.resetSelectProduct();
   }
 
-  const cards = document.querySelectorAll('.visualisation-3d.top .card');
-  setActiveCard(cards[0]);
+  function setActiveCard(newCard) {
+    const oldCard = newCard.closest('.cards').querySelector('.card.active');
+    if (oldCard) oldCard.classList.remove('active');
+
+    newCard.classList.add('active');
+    img3d.src = newCard.dataset.srcLarge;
+
+    prodLink.href = newCard.dataset.productLink;
+  }
+
+  function setSelectColor(newCard) {
+    const interiorType = newCard.dataset.interiorType;
+
+    selectColor.querySelector(`.colors[data-interior-type="${interiorType}"]`).classList.remove('hidden');
+  }
+
+  window.resetSelectColor = function () {
+    selectColor.querySelector('.select-placeholder').classList.remove('hidden');
+    selectColor.querySelector('.select-title').classList.add('hidden');
+
+    const activeColor = selectColor.querySelector('.color.active');
+    if (activeColor) activeColor.classList.remove('active');
+
+    const activeColors = selectColor.querySelector('.colors:not(.hidden)');
+    if (activeColors) activeColors.classList.add('hidden');
+  };
+
+  window.resetSelectCollection = function () {
+    selectCollection.querySelector('.summary').classList.add('disabled');
+    selectCollection.querySelector('.select-placeholder').classList.remove('hidden');
+    selectCollection.querySelector('.select-title').classList.add('hidden');
+
+    const activeCollection = selectCollection.querySelector('.collection.active');
+    if (activeCollection) activeCollection.classList.remove('active');
+  };
+
+  window.resetSelectProduct = function () {
+    selectProduct.querySelector('.summary').classList.add('disabled');
+    selectProduct.querySelector('.select-placeholder').classList.remove('hidden');
+    selectProduct.querySelector('.select-title').classList.add('hidden');
+    selectProduct.querySelector('.products').innerHTML = '';
+  };
+
+  run(document.querySelector('.visualisation-3d.top .card'));
 
   document.addEventListener('click', function (e) {
     const newCard = e.target.closest('.visualisation-3d.top .card:not(.active)');
-    setActiveCard(newCard);
+    run(newCard);
   });
+
 });
--- resources/scripts/page/3d/bottom.js
+++ resources/scripts/page/3d/bottom.js
@@ -1,25 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const selects = document.querySelector('.visualisation-3d.bottom .selects');
+
+  const selectCollection =  document.querySelector('.visualisation-3d.bottom .select-collection');
+  const selectColor = document.querySelector('.visualisation-3d.bottom .select-color');
+  const selectProduct = document.querySelector('.visualisation-3d.bottom .select-product');
+  const img3d = document.querySelector('.visualisation-3d .wrapper-3d img');
+  const prodLink = document.querySelector('.visualisation-3d .to-product');
 
   document.addEventListener('click', function (e) {
-    const placeHolder = e.target.closest('.visualisation-3d.bottom .select-placeholder-opened');
+    const select = e.target.closest('.visualisation-3d.bottom .select');
 
-    if (!placeHolder) return true;
+    if (!select) return true;
 
-    placeHolder.closest('.select').classList.remove('active');
-  });
+    const oldSelect = document.querySelector('.visualisation-3d.bottom .select[open]');
 
-  document.addEventListener('click', function (e) {
-    const placeHolder = e.target.closest('.visualisation-3d.bottom .select-placeholder-closed');
-
-    if (!placeHolder) return true;
-
-    const newSelect = placeHolder.closest('.select');
-    const oldSelect = selects.querySelector('.select.active');
-
-    newSelect.classList.add('active');
-
-    if (oldSelect) oldSelect.classList.remove('active');
+    if (oldSelect && select !== oldSelect) oldSelect.open = false;
   });
 
   document.addEventListener('click', function (e) {
@@ -27,9 +21,68 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (select) return true;
 
-    const oldSelect = selects.querySelector('.select.active');
+    const oldSelect = document.querySelector('.visualisation-3d.bottom .select[open]');
 
-    if (oldSelect) oldSelect.classList.remove('active');
+    if (oldSelect) oldSelect.open = false;
+  });
+
+  document.addEventListener('click', function (e) {
+    const summary = e.target.closest('.visualisation-3d.bottom .select .summary.disabled');
+
+    if (!summary) return true;
+
+    e.preventDefault();
+  });
+
+  document.addEventListener('click', function (e) {
+    const newColor = e.target.closest('.visualisation-3d.bottom .select-color .color:not(.active)');
+
+    if (!newColor) return true;
+
+    const oldColor = selectColor.querySelector('.color.active');
+
+    newColor.classList.add('active');
+    newColor.closest('.select').open = false;
+
+    if (oldColor) oldColor.classList.remove('active');
+
+    img3d.src = newColor.dataset.srcLarge;
+
+    // todo set summary
+
+    window.resetSelectCollection();
+    window.resetSelectProduct();
+
+    selectColor.querySelector('.select-placeholder').classList.add('hidden');
+    selectColor.querySelector('.select-title').textContent = newColor.dataset.title;
+    selectColor.querySelector('.select-title').classList.remove('hidden');
+
+    selectCollection.querySelector('.summary').classList.remove('disabled');
+  });
+
+  document.addEventListener('click', function (e) {
+    const newCollection = e.target.closest('.visualisation-3d.bottom .select-collection .collection:not(.active)');
+
+    if (!newCollection) return true;
+
+    const oldCollection = selectCollection.querySelector('.collection.active');
+
+    newCollection.classList.add('active');
+    newCollection.closest('.select').open = false;
+
+    if (oldCollection) oldCollection.classList.remove('active');
+
+    // todo set summary
+
+    window.resetSelectProduct();
+
+    selectCollection.querySelector('.select-placeholder').classList.add('hidden');
+    selectCollection.querySelector('.select-title').textContent = newCollection.textContent;
+    selectCollection.querySelector('.select-title').classList.remove('hidden');
+
+    // todo ajax
+
+    selectProduct.querySelector('.summary').classList.remove('disabled');
   });
 
 });
